afea56488dca096591acec9b14c61a06308c20dd
Giuseppe Capizzi <g.capizzi@gmail.com>
2012-10-24 16:44:04 +0200

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+endpath="$HOME/.dotfiles"
+
+echo "Bootstrap started\n"
+
+echo "> clone repo\n"
+git clone http://github.com/gcapizzi/dotfiles.git $endpath
+
+echo "\n> create directories\n"
+mkdir -p $endpath/vim
+
+echo "> set up symlinks\n"
+ln -s $endpath/vim $HOME/.vim
+ln -s $endpath/vimrc $HOME/.vimrc
+ln -s $endpath/zshrc $HOME/.zshrc
+ln -s $endpath/gitconfig $HOME/.gitconfig
+ln -s $endpath/cvsignore $HOME/.cvsignore
+
+if [ ! -e $HOME/.vim/bundle/vundle ]; then
+    echo "> install Vundle\n"
+    git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
+
+echo "\n> update/install plugins using Vundle\n"
+vim +BundleInstall! +BundleClean +qall
+
+echo "\n> install oh-my-zsh\n"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+chsh -s `which zsh`
