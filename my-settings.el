839c0f2b4c64933d801e8c1102d736e011463df6
Giuseppe Capizzi <g.capizzi@gmail.com>
2015-02-23 10:59:11 +0100

    Add Emacs/Prelude config

---
+++ my-settings.el
@@ -0,0 +1,13 @@
+;; theme
+(disable-theme 'zenburn)
+(setq prelude-theme 'solarized-dark)
+
+;; font
+(set-face-attribute 'default nil :family "Menlo")
+(set-face-attribute 'default nil :height 185)
+
+;; scrollbar
+(set-scroll-bar-mode nil)
+
+;; line numbers
+(global-linum-mode t)
