45eef6a82f23a4243eda3747c249a8fcf1fc5a3d
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-08-24 18:21:54 +0100

    Add zplug install/update to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,3 +17,12 @@ ln -sf $endpath/zshrc $HOME/.zshrc
 ln -sf $endpath/gitconfig $HOME/.gitconfig
 ln -sf $endpath/gitignore $HOME/.gitignore
 ln -sf $endpath/tmux.conf $HOME/.tmux.conf
+
+if zplug check; then
+    echo "\n> zplug update\n"
+    zplug update
+else
+    echo "\n> zplug install\n"
+    zplug install
+fi
+
