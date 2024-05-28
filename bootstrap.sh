975b17499d7919cbc3555f8349034153f9f15ae9
Giuseppe Capizzi <g.capizzi@gmail.com>
2024-05-28 12:37:40 +0100

    No need to remote the old Alacritty config anymore

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,6 @@ ln -sf "$endpath/zshrc" "$HOME/.zshrc"
 ln -sf "$endpath/gitconfig" "$HOME/.gitconfig"
 ln -sf "$endpath/gitignore" "$HOME/.gitignore"
 ln -sf "$endpath/tmux.conf" "$HOME/.tmux.conf"
-rm -f "$HOME/.alacritty.yml"
 ln -sf "$endpath/alacritty.toml" "$HOME/.alacritty.toml"
 
 echo "> zplug install/update"
