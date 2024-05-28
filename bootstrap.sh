2b49f0ecd22adb9c909b2a4376538af5e8af7543
Giuseppe Capizzi <g.capizzi@gmail.com>
2024-05-28 13:31:53 +0100

    Handle existing ~/.zplug

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,5 +12,9 @@ ln -sf "$dir/tmux.conf" "$HOME/.tmux.conf"
 ln -sf "$dir/alacritty.toml" "$HOME/.alacritty.toml"
 
 echo "> zplug install/update"
-git clone https://github.com/zplug/zplug.git ~/.zplug
-/bin/zsh -c "source ~/.zshrc && ((zplug check && zplug install) || zplug update)"
+if [[ -d "$HOME/.zplug" ]]; then
+	/bin/zsh -c "source ~/.zshrc && zplug update"
+else
+	git clone https://github.com/zplug/zplug.git ~/.zplug
+	/bin/zsh -c "source ~/.zshrc && zplug check && zplug install"
+fi
